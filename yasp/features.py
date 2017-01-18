299a8ad1ab6587f282de6cc8fd6f5ff13c061138
Kevin Pitolin <kevin@Kevins-MacBook-Pro.local>
2017-01-18 20:33:33 +0100

    added some utils methods to add an extract features, implemented compute_hotkeys_distribution_feature. Some of it was not tested XD

--- yasp/features.py
+++ yasp/features.py
@@ -1,24 +1,51 @@
-from sklearn import tree
-import csvHandler
 
+
+def extract_race_feature(raw_features):
+	"""
+	You must provide it with extract_rows_from_CSV()["data"]
+	"""
+	feature_list = []
+	for i in range(0,len(raw_features)):
+		feature_list.append(len(raw_features[i][0]))
+
+	return feature_list
+
+def add_single_feature(data, single_feature_list):
+	"""
+	Add a feature  [valueGameData1, valueGameData2,...] to the list of features
+	"""
+
+	for i in range(0,len(single_feature_list)):
+		data[i].append(single_feature_list[i])
+
+	return data
+
+def extract_string_feature(data_list, string_feature):
+	"""
+	You must provide extract_rows_from_CSV()["data"] as data_list 
+	string_feature is s or sBase or sMineral or a hotkey
+	"""
+	feature_list = []
+	for i in range(0,len(data_list)):
+		count_of_features = 0
+		for feature in data_list[i]:
+			if string_feature == feature:
+				count_of_features = count_of_features + 1
+		feature_list.append(count_of_features)
+			# print feature_list
+	return feature_list
 
 
 # Features computation to determine specialization of experts
 
-def compute_user_mean_speed_feature(feature_list):
-	"""
-	Compute user speed
-
-	Format of list [action1,frame1, ...]
-	Returns a float with the mean speed
-	"""
-	return len(feature_list)-1/2*(int(feature_list[-1]) - int(feature_list[2]))
 
 
 
 def compute_hotkeys_distribution_feature(list_of_feature_list):
 	"""
 	Hotkeys distribution
+
+	List of [[nb_hotkey0_used,nb_hotkey1_used, nb_hotkey2_used, nb_hotkey3_used, ... ] ,...]
 	"""
 	list_of_hotkey_dict = []
 	for lists in list_of_feature_list:
@@ -29,26 +56,34 @@ def compute_hotkeys_distribution_feature(list_of_feature_list):
 			# We verify the element is a used hotkey 
 			if "hotkey" in feature and "2" in feature :
 				feature = feature [:-1]
-				print feature
 				if feature in hotkey_dic:
 					hotkey_dic[feature] = hotkey_dic[feature] + 1
 				else:
 					hotkey_dic[feature] = 1
 
 		hotkey_list = []
-		for x in xrange(0,9):
+		for x in xrange(0,10):
 			if "hotkey" + str(x) in hotkey_dic:
-				print "hotkey" + str(x)
-				hotkey_list.insert(hotkey_dic["hotkey" + str(x)],x) 
+				hotkey_list.insert(x, hotkey_dic["hotkey" + str(x)]) 
 			else:
-				hotkey_list.insert(0,x) 
+				hotkey_list.insert(x,0) 
 
 		list_of_hotkey_dict.append(hotkey_list)
 
 
 	return list_of_hotkey_dict
 
+# The following methods were not tested
+
 	
+def compute_user_mean_speed_feature(feature_list):
+	"""
+	Compute user speed
+
+	Format of list [action1,frame1, ...]
+	Returns a float with the mean speed
+	"""
+	return len(feature_list)-1/2*(int(feature_list[-1]) - int(feature_list[2]))
 
 
 def compute_repetition_pattern_of_hotkeys_feature():
--- yasp/firstPredictor.py
+++ yasp/firstPredictor.py
@@ -1,7 +1,6 @@
-# nombre de selection de chaque type
-# nombre de hotkey1
-# vitesse de jeu
-
+import random
+import math
+import features
 from sklearn import tree
 from csvHandler import *
 from  sklearn.cross_validation import train_test_split
@@ -28,17 +27,33 @@ def calculateFeatures(data):
 		features.append(game_features)
 	return features
 
-features_labels = extract_rows_from_CSV()
-Y = features_labels["labels"]
-X = calculateFeatures(features_labels["data"])
 
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = .5)
+features_list = extract_rows_from_CSV()
+data_hotkeys = features.compute_hotkeys_distribution_feature(features_list["data"])
+print data_hotkeys[0]
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X_train, Y_train)
-data_test = extract_rows_from_CSV('../../datayasp/test.csv')
-features_test = calculateFeatures(data_test["data"])
-players_test = data_test["labels"]
-predictions = clf.predict(X_test)
-#write_to_submit_CSV(players_test,predictions)
-print accuracy_score(Y_test, predictions)
+features.add_single_feature(data_hotkeys, features.extract_string_feature(features_list["data"],"sBase"))
+print data_hotkeys[0]
+
+features.add_single_feature(data_hotkeys, features.extract_string_feature(features_list["data"],"sMineral"))
+print data_hotkeys[1]
+
+features.add_single_feature(data_hotkeys, features.extract_race_feature(features_list["data"]))
+print data_hotkeys[0]
+
+
+# #features_labels = extract_rows_from_CSV()
+# Y = features_list["labels"]
+# X = calculateFeatures(data_hotkeys)
+
+# X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = .33, random_state=int(random.uniform(0,100)))
+
+# clf = tree.DecisionTreeClassifier()
+# clf = clf.fit(X_train, Y_train)
+# data_test = extract_rows_from_CSV('../../datayasp/test.csv')
+# features_test = features.compute_hotkeys_distribution_feature(data_test["data"])
+# players_test = data_test["labels"]
+# predictions = clf.predict(X_test)
+# #write_to_submit_CSV(players_test,predictions)
+# print accuracy_score(Y_test, predictions)
+ 
